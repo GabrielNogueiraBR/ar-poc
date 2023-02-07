2455e8477a43602781d597d2488d6bea55e6c9be
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-02-07 08:37:41 -0300

    feat: adding THREEjs and ARjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import * as THREE from "three";
+import * as THREEx from "ar-js-org/three.js/build/ar-threex.js";
 
 function App() {
-  return <div>Olá mundo</div>;
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.Camera();
+
+    scene.add(camera);
+
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true,
+    });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const ArToolkitSource = new THREEx.ArToolkitSource({
+      sourceType: "webcam",
+    });
+
+    ArToolkitSource.init(function () {
+      setTimeout(function () {
+        ArToolkitSource.onResizeElement();
+        ArToolkitSource.copyElementSizeTo(renderer.domElement);
+      }, 2000);
+    });
+
+    const ArToolkitContext = new THREEx.ArToolkitContext({
+      cameraParametersUrl: "/camera_para.dat",
+      detectionMode: "color_and_matrix",
+    });
+
+    ArToolkitContext.init(function () {
+      camera.projectionMatrix.copy(ArToolkitContext.getProjectionMatrix());
+    });
+
+    const ArMarkerControls = new THREEx.ArMarkerControls(
+      ArToolkitContext,
+      camera,
+      {
+        type: "barcode",
+        barcodeValue: "TESTE-AR-POC-THREEJS-ARJS",
+        changeMatrixMode: "cameraTransformMatrix",
+      }
+    );
+
+    scene.visible = false;
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshNormalMaterial({
+      transparent: true,
+      opacity: 0.8,
+      side: THREE.DoubleSide,
+    });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.y = geometry.parameters.height / 2;
+    scene.add(cube);
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      ArToolkitContext.update(ArToolkitSource.domElement);
+      scene.visible = camera.visible;
+
+      renderer.render(scene, camera);
+    }
+
+    animate();
+  }, []);
+
+  return <canvas></canvas>;
 }
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="react-scripts" />
+
+declare module '@ar-js-org/ar.js'
+declare module 'arjs'
+declare module 'ar-js-org'
+declare module 'ar-js-org/three.js/build/ar-threex.js'
